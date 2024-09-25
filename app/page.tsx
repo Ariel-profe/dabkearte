9930ba2cbec9dc5de2ab1c2b5f2ee3f1b8420b74
Ariel Elias <eliasariel@live.com>
2024-09-25 12:20:38 -0300

    revert team component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { About } from "@/components/sections/About"
 import { Galery } from "@/components/sections/Galery"
 import { Hero } from "@/components/sections/Hero"
 import { Surprise } from "@/components/sections/Surprise"
-import { Team } from "@/components/sections/Team"
 import { Team2 } from "@/components/sections/Team2"
 
 const Homepage = () => {
