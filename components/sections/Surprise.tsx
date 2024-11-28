67eeb2c786e526e754a888147adaa70ce926348b
Ariel Elias <eliasariel@live.com>
2024-11-28 17:08:12 -0300

    feat canvas cursos added

--- components/sections/Surprise.tsx
+++ components/sections/Surprise.tsx
@@ -1,12 +1,16 @@
 "use client";
 
-import { SparklesText } from "../ui/sparkles-text";
-import { TextHoverEffect } from "../ui/text-hover-effect";
+import useCanvasCursor from "@/hooks/use-canvas-cursor";
+import { SparklesText } from "@/components/ui/sparkles-text";
+import { TextHoverEffect } from "@/components/ui/text-hover-effect";
 
 export function Surprise() {
 
+    useCanvasCursor()
+
     return (
         <section className="py-20 relative bg-slate-950/50">
+            <canvas className="pointer-events-none fixed inset-0 z-50" id="canvas"/>
             <div className="container mx-auto">
 
             <SparklesText text="Nuestro show más esperado" className="text-center lg:text-end" />
